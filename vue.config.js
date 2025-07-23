613b899367149118939dcd05a01509b25189013b
Prathmesh3742 <santoshpandey9484@gmail.com>
2025-07-23 10:25:54 +0530

    veu change

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
 module.exports = {
   publicPath: 'https://github.com/Prathmesh3742/prac2'
 }
